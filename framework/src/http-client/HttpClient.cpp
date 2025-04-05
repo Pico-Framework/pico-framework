de6475188386874025b95db74577acd004a43477
Ian Archbell <ian@archbell.com>
2025-04-05 13:13:16 -0700

    mbedtls changes

--- framework/src/http-client/HttpClient.cpp
+++ framework/src/http-client/HttpClient.cpp
@@ -17,6 +17,16 @@ TRACE_INIT(HttpClient);
 #include "mbedtls/ctr_drbg.h"
 #endif
 
+static int mbedtls_send_callback(void* ctx, const unsigned char* buf, size_t len) {
+    TcpConnectionSocket* socket = static_cast<TcpConnectionSocket*>(ctx);
+    return socket->send(reinterpret_cast<const char*>(buf), len);
+}
+
+static int mbedtls_recv_callback(void* ctx, unsigned char* buf, size_t len) {
+    TcpConnectionSocket* socket = static_cast<TcpConnectionSocket*>(ctx);
+    return socket->recv(reinterpret_cast<char*>(buf), len);
+}
+
 bool HttpClient::get(const std::string& url, HttpClientResponse& response) {
     TRACE("HttpClient", "Making request to %s", url.c_str());
     // Very minimal URL parsing (http/https, host, path)
@@ -97,82 +107,79 @@ bool HttpClient::getPlain(const std::string& host, const std::string& path, Http
 
 #if PICO_HTTP_CLIENT_ENABLE_TLS
 bool HttpClient::getTls(const std::string& host, const std::string& path, HttpClientResponse& response) {
-    mbedtls_net_context net;
     mbedtls_ssl_context ssl;
     mbedtls_ssl_config conf;
     mbedtls_entropy_context entropy;
     mbedtls_ctr_drbg_context ctr_drbg;
-    const char* pers = "http_client";
 
-    mbedtls_net_init(&net);
-    mbedtls_ssl_init(&ssl);
-    mbedtls_ssl_config_init(&conf);
-    mbedtls_ctr_drbg_init(&ctr_drbg);
-    mbedtls_entropy_init(&entropy);
+    const char* pers = "pico_https_client";
+    TcpConnectionSocket socket;
 
-    if (mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy,
-                               (const unsigned char*)pers, strlen(pers)) != 0) {
+    if (!socket.connect(host.c_str(), 443)) {
+        printf("TLS: Socket connection failed\n");
         return false;
     }
 
-    if (mbedtls_net_connect(&net, host.c_str(), "443", MBEDTLS_NET_PROTO_TCP) != 0) {
+    mbedtls_ssl_init(&ssl);
+    mbedtls_ssl_config_init(&conf);
+    mbedtls_entropy_init(&entropy);
+    mbedtls_ctr_drbg_init(&ctr_drbg);
+
+    if (mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy,
+                               reinterpret_cast<const unsigned char*>(pers), strlen(pers)) != 0) {
+        printf("TLS: Failed to seed DRBG\n");
         return false;
     }
 
     if (mbedtls_ssl_config_defaults(&conf,
-        MBEDTLS_SSL_IS_CLIENT,
-        MBEDTLS_SSL_TRANSPORT_STREAM,
-        MBEDTLS_SSL_PRESET_DEFAULT) != 0) {
+                                    MBEDTLS_SSL_IS_CLIENT,
+                                    MBEDTLS_SSL_TRANSPORT_STREAM,
+                                    MBEDTLS_SSL_PRESET_DEFAULT) != 0) {
+        printf("TLS: SSL config defaults failed\n");
         return false;
     }
 
-    mbedtls_ssl_conf_authmode(&conf, MBEDTLS_SSL_VERIFY_NONE);
+    mbedtls_ssl_conf_authmode(&conf, ALTCP_MBEDTLS_AUTHMODE); // Controlled via build config
     mbedtls_ssl_conf_rng(&conf, mbedtls_ctr_drbg_random, &ctr_drbg);
 
     if (mbedtls_ssl_setup(&ssl, &conf) != 0) {
+        printf("TLS: SSL setup failed\n");
         return false;
     }
 
-    mbedtls_ssl_set_bio(&ssl, &net, mbedtls_net_send, mbedtls_net_recv, nullptr);
+    mbedtls_ssl_set_bio(&ssl, &socket, mbedtls_send_callback, mbedtls_recv_callback, nullptr);
+    mbedtls_ssl_set_hostname(&ssl, host.c_str());
 
-    if (mbedtls_ssl_handshake(&ssl) != 0) {
+    while (true) {
+        int ret = mbedtls_ssl_handshake(&ssl);
+        if (ret == 0) break;
+        if (ret != MBEDTLS_ERR_SSL_WANT_READ && ret != MBEDTLS_ERR_SSL_WANT_WRITE) {
+            printf("TLS: Handshake failed: -0x%x\n", -ret);
+            return false;
+        }
+    }
+
+    std::string request = "GET " + path + " HTTP/1.1\r\nHost: " + host + "\r\nConnection: close\r\n\r\n";
+    if (mbedtls_ssl_write(&ssl, reinterpret_cast<const unsigned char*>(request.c_str()), request.size()) <= 0) {
+        printf("TLS: Write failed\n");
         return false;
     }
 
-    std::ostringstream req;
-    req << "GET " << path << " HTTP/1.1\r\n"
-        << "Host: " << host << "\r\n"
-        << "Connection: close\r\n\r\n";
+    char buf[1024];
+    int len;
+    response.body.clear();
 
-    mbedtls_ssl_write(&ssl, (const unsigned char*)req.str().c_str(), req.str().size());
-
-    std::string raw;
-    char buffer[1024];
-    int len = 0;
-    while ((len = mbedtls_ssl_read(&ssl, (unsigned char*)buffer, sizeof(buffer))) > 0) {
-        raw.append(buffer, len);
+    while ((len = mbedtls_ssl_read(&ssl, reinterpret_cast<unsigned char*>(buf), sizeof(buf) - 1)) > 0) {
+        buf[len] = '\0';
+        response.body.append(buf);
     }
 
-    mbedtls_ssl_close_notify(&ssl);
-    mbedtls_net_free(&net);
+    // TODO: Parse status code and headers from response.body if needed
+
     mbedtls_ssl_free(&ssl);
     mbedtls_ssl_config_free(&conf);
     mbedtls_ctr_drbg_free(&ctr_drbg);
     mbedtls_entropy_free(&entropy);
-
-    std::string headerText;
-    std::string body = extractHeadersAndBody(raw, headerText);
-    response.headers = HttpParser::parseHeaders(headerText);
-    response.statusCode = HttpParser::parseStatusCode(headerText);
-
-    auto it = response.headers.find("transfer-encoding");
-    if (it != response.headers.end() && it->second == "chunked") {
-        ChunkedDecoder decoder;
-        decoder.feed(body);
-        response.body = decoder.getDecoded();
-    } else {
-        response.body = body;
-    }
 
     return true;
 }
