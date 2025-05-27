05a7fa121a86226efde24bd8b911cff0c17c6a7c
Ian Archbell <ian@archbell.com>
2025-05-27 07:06:06 -0700

    cmake

--- cmake/framework_application.cmake
+++ cmake/framework_application.cmake
@@ -89,6 +89,7 @@ endif()
 
 if(FLASH_METHOD STREQUAL "openocd")
     if(NOT DEFINED OPENOCD_EXECUTABLE)
+        message(STATUS "System PATH seen by CMake: $ENV{PATH}")
         find_program(OPENOCD_EXECUTABLE NAMES openocd)
     endif()
     if(NOT OPENOCD_EXECUTABLE)
