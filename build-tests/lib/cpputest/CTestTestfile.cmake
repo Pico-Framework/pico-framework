415b1cac9087bed91cc6b9ad5f000bf707df7368
Ian Archbell <ian@archbell.com>
2025-06-13 11:32:12 -0700

    removed build-tests

--- build-tests/lib/cpputest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ianarchbell/Documents/pico-framework/tests/lib/cpputest
-# Build directory: /Users/ianarchbell/Documents/pico-framework/build-tests/lib/cpputest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
--- build-tests/lib/cpputest/src/CppUTest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ianarchbell/Documents/pico-framework/tests/lib/cpputest/src/CppUTest
-# Build directory: /Users/ianarchbell/Documents/pico-framework/build-tests/lib/cpputest/src/CppUTest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
